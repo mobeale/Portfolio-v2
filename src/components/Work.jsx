51e8af452ebd0c42ddaf6450d926fedfabad5d5a
Mat <mat.beale@exonar.com>
2017-03-22 23:03:02 +0000

    Commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,13 +10,15 @@ const Work = React.createClass({
                     <div className="center-box text-center work">
                       <Typist avgTypingDelay={50} startDelay={1000} cursor={{ hideWhenDone: true, blink: true }}>
                         <div className="row">
-                          <div className="col-xs-6">
+                          <div className="col-sm-12">
                             <h3>Skills:</h3>
                             <li>Javascript</li>
                             <li>React & Redux</li>
                             <li>Java</li>
                           </div>
-                          <div className="col-xs-6">
+                        </div>
+                        <div className="row" style={{marginTop: "20px"}}>
+                          <div className="col-sm-12">
                             <h3>Interests:</h3>
                             <li>Computing</li>
                             <li>Fitness</li>
@@ -29,12 +31,6 @@ const Work = React.createClass({
                             <h3>Employment:</h3>
                             <li>TalentSee - Front end Developer 2015 > 2016</li>
                             <li>Exonar - Jr Software Engineer 2016 > present</li>
-                          </div>
-                        </div>
-                        <div className="row" style={{marginTop: "20px"}}>
-                          <div className="col-sm-12">
-                            <h3>Education:</h3>
-                            <li>Quantity Surveying 2:1 - Portsmouth University</li>
                           </div>
                         </div>
                         </Typist>
